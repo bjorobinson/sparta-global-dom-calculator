b18ce2b2f61937d60a9a3aa6c9648c89a6d97a5b
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-01-31 16:47:17 +0000

    Made function to create a numerical button. Assigns a value to numerical buttons

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,28 @@
-var display = document.getElementById('container')
-//Add Buttons
-var numbers = document.getElementsByClassName('buttonNum');
-var button7 = numbers[0];
-button7.addEventListener('click',handleClick);
-//Handle buttons
-function handleClick(){
-  console.log('Click!');
+//FUNCTIONS
+function makeButtonNum(newButton, index, val){
+  newButton = numbers[index];
+  newButton.addEventListener('click',handleClick);
+  function handleClick(){
+    console.log(val);
+  }
+  return newButton;
 }
+
+//PROGRAM START
+var display = document.getElementById('container')
+//Add Number Buttons
+var numbers = document.getElementsByClassName('buttonNum');
+var button7 = makeButtonNum(button7,0,7);
+var button8 = makeButtonNum(button8,1,8);
+var button9 = makeButtonNum(button9,2,9);
+var button4 = makeButtonNum(button4,3,4);
+var button5 = makeButtonNum(button5,4,5);
+var button6 = makeButtonNum(button6,5,6);
+var button1 = makeButtonNum(button1,6,1);
+var button2 = makeButtonNum(button2,7,2);
+var button3 = makeButtonNum(button3,8,3);
+
+
 //Console logs for testing
 console.log(numbers);
 console.log(button7);
